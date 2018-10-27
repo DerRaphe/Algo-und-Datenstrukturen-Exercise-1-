f2666b433fe6ae3b34caa73eb05f3b07ea8957d8
DerRaphe <DerRaphe@DESKTOP-8OUUGMU>
2018-10-27 14:04:51 +0200

    Quick Sort für Exercise 3

---
+++ src/DivideAndConquerQuickSort/DivideAndConquerableParallel.java
@@ -0,0 +1,55 @@
+package DivideAndConquerQuickSort;
+
+
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.ArrayList;
+
+public interface DivideAndConquerableParallel<OutputType> extends DivideAndConquerable<OutputType> {
+  boolean threadPoolIsInitailised();
+
+  boolean isTooSmallForMultiThreading();
+
+  int numberOfThreadPool();
+
+  void setThreadPool();
+
+  ForkJoinPool getThreadPool();
+
+  ReentrantLock lock = new ReentrantLock();
+
+  @Override
+  default OutputType divideAndConquer() {
+    if (this.isBasic())
+      return (OutputType) this.baseFun();
+
+    List<? extends DivideAndConquerable<OutputType>> subcomponents = this.decompose();
+
+    List<OutputType> intermediateResults = new ArrayList<OutputType>(subcomponents.size());
+    if (!threadPoolIsInitailised())
+      setThreadPool();
+
+    for (int i = 0; i < subcomponents.size(); i++) {
+      if (getThreadPool().getPoolSize() > numberOfThreadPool() || isTooSmallForMultiThreading()) {
+        intermediateResults.add(subcomponents.get(i).divideAndConquer());
+      } else {
+        DivideAndConquerable<OutputType> giveToPool = subcomponents.get(i);
+        getThreadPool().submit(() -> {
+          intermediateResults.add(giveToPool.divideAndConquer());
+        });
+      }
+    }
+
+    try {
+      getThreadPool().shutdown();
+      while (!getThreadPool().awaitTermination(24L, TimeUnit.HOURS)) {
+
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    return (OutputType) recombine(intermediateResults);
+  }
+}
---
+++ src/DivideAndConquerQuickSort/ListFactory.java
@@ -0,0 +1,25 @@
+package DivideAndConquerQuickSort;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ListFactory {
+  private int listLength = 300;
+  private List<Integer> listToSort;
+  private Random rand = new Random();
+
+  public ListFactory(int listLength) {
+    this.listLength = listLength;
+    listToSort = new ArrayList<Integer>();
+    for (int i = 0; i < this.listLength; i++) {
+      listToSort.add(rand.nextInt(300) + 1);
+    }
+  }
+
+  public List<Integer> giveListToSort(int length) {
+    return listToSort.subList(0, length);
+  }
+
+}
